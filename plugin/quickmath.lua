6375c6c0aac40c40e7d2e2cf42b2d29a9b143650
StianM <stian.myklebostad@schibsted.com>
2022-02-15 10:41:17 +0100

    remove command declaration `:QMSelectOutput`

--- plugin/quickmath.lua
+++ plugin/quickmath.lua
@@ -1,3 +1,2 @@
 -- to have a clean 100% lua statistics in Github ;) I know it's perfectionism
 vim.cmd [[command! Quickmath lua require("quickmath").StartSession()]]
-vim.cmd [[command! QMSelectOutput lua require("quickmath").go_eol()]]
